697200189621c7cbdfb04870d7ebb46c4cd8b1d3
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-02-09 19:14:17 -0300

    att

--- extrairdados/extrair.py
+++ extrairdados/extrair.py
@@ -1,19 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('http://coreyms.com').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-article = soup.find('article')
+csv_file = open('csm_scrape.csv', 'w')
 
-#print(article.prettify())
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline','summary', 'video_link'])
 
-# headline = article.h2.a.text
-# print(headline)
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
 
-# summary = article.find('div', class_='entry-content').p.text
-# print(summary)
+    summary = article.find('div', class_='entry-content').p.text
+    print(summary)
 
-vid_src = article.find('iframe', class_= 'youtube-player')
-print(vid_src)
+    try:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
+
+        vid_id = vid_src.split('/')[4]
+        vid_id = vid_id.split('?')[0]
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+    except Exception as e:
+        yt_link = None
+
+    print(yt_link)
+    print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
