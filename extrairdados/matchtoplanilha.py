8f71f6c2483ce14c1c6c2c8e43f70664918bbc22
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-02-22 10:05:37 -0300

    new

--- extrairdados/matchtoplanilha.py
+++ extrairdados/matchtoplanilha.py
@@ -2,37 +2,55 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-dia = int(input("Digite o dia 1 ou 2 etc: "))
-numpartidas = int(input("Digite o numero de partidas do dia: "))
-numpartidasold = numpartidas
-numpartidas = numpartidas * dia
-
 cont = 0
 i = 0
 j = 0
 
 
-source = requests.get('https://lol.gamepedia.com/CBLOL/2021_Season/Split_1/Scoreboards').text
+source = requests.get('https://lol.gamepedia.com/LPL/2021_Season/Spring_Season/Scoreboards/Week_5_(3)').text
 
 soup = BeautifulSoup(source, 'html.parser')
 times = soup.find_all('span', "teamname")
+kills = soup.find_all('div', "sb-header-Kills")
 
+numpartidas = 15
 listatime = []
-partidas = []
+partidas = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+numkills = []
+numpartidaskills = [0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]
 
-for i in range(0,numpartidasold):
-    partidas.append([0,0])
 
 print(partidas)
 
+for time in times:
+    listatime.append(time.text)
+
+for l in range(0,numpartidas):
+    for c in range(0,2):
+        partidas[l][c] = listatime[i]
+        i = i + 1
 
 
+for kill in kills:
+    numkills.append(kill.text)
+
+for linha in range(0,numpartidas):
+    for coluna in range(0,2):
+        numpartidaskills[linha][coluna] = numkills[j]
+        j = j + 1
 
 
-print(listatime)
-
+print(numpartidaskills)
 """with open('newFile.csv', 'a') as csvfile:
     wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
     for word in partidas:
         wr.writerow([None,[word]])
 """
+
+with open('newFile.csv', 'a', newline="") as csvfile:
+
+    wr = csv.writer(csvfile,quoting=csv.QUOTE_ALL)
+    for word in partidas:
+        wr.writerow(word)
+    for word in numpartidaskills:
+        wr.writerow(word)
