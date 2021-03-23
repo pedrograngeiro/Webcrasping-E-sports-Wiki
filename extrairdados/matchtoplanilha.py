3ee6475d9adb90ae6a15c8dead75f291cadf89b3
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-03-23 20:22:30 -0300

    new

--- extrairdados/matchtoplanilha.py
+++ extrairdados/matchtoplanilha.py
@@ -7,17 +7,17 @@ i = 0
 j = 0
 
 
-source = requests.get('https://lol.gamepedia.com/CBLOL/2021_Season/Split_1/Scoreboards/Week_6').text
+source = requests.get('https://lol.gamepedia.com/LPL/2021_Season/Spring_Season/Scoreboards/Week_6_(3)').text
 
 soup = BeautifulSoup(source, 'html.parser')
 times = soup.find_all('span', "teamname")
 kills = soup.find_all('div', "sb-header-Kills")
 
-numpartidas = 10
+numpartidas = 14
 listatime = []
-partidas = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+partidas = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 numkills = []
-numpartidaskills = [0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]
+numpartidaskills = [0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]
 
 
 print(partidas)
@@ -47,7 +47,7 @@ print(numpartidaskills)
         wr.writerow([None,[word]])
 """
 
-with open('cblolstats.csv', 'a', newline="") as csvfile:
+with open('newFile copia.csv', 'a', newline="") as csvfile:
 
     wr = csv.writer(csvfile,quoting=csv.QUOTE_ALL)
     for word in partidas:
