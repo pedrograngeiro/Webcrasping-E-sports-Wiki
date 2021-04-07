d5f6f1a0577b7ff2e2796bfd18b412f296256fe9
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-06 23:14:37 -0300

    Update otimizado.py

--- extrairdados/otimizado.py
+++ extrairdados/otimizado.py
@@ -6,37 +6,40 @@ import csv
 i = 0
 j = 0
 
-
-source = requests.get('https://lol.fandom.com/wiki/CBLOL/2021_Season/Split_1_Playoffs/Scoreboards').text
-
-soup = BeautifulSoup(source, 'html.parser')
-pesquisadefinida = soup.find_all('div', "sb-header-Kills")
-
-numpartidas = int(input("Digite o numero de partidas: "))
+# inicializei listas vazias
 partidas = []
-
-for i in range(0, numpartidas):
-    partidas.append([0,0])
-
 numdados = []
-
 numDadosTime1Time2 = []
 
-for i in range(0, numpartidas):
+
+source = requests.get('https://lol.fandom.com/wiki/CBLOL/2021_Season/Split_1_Playoffs/Scoreboards').text #chama código fonte do link e armazena na variavél
+
+soup = BeautifulSoup(source, 'html.parser') #separa o html
+pesquisadefinida = soup.find_all('div', "sb-header-Kills") #pesquisa os atributos que você quer
+
+numpartidas = int(input("Digite o numero de partidas: ")) #armazena o número de jogos do dia
+
+for i in range(0, numpartidas): #loop para add os elementos [0,0] dentro da lista até o numero de partidas definidas
+    partidas.append([0,0])
+
+for i in range(0, numpartidas): #loop para add os elementos [0,0] do time 1 e time 2 dentro da variável "numDadosTime1Time2"
     numDadosTime1Time2.append([0,0])
 
-for dados in pesquisadefinida:
-    numdados.append(dados.text)
+#variavel "dados" é por padrão inicializada em 0
+for dados in pesquisadefinida: # loop percorrendo de 0 até o fim de "pesquisadefinida"
+    numdados.append(dados.text) # add dentro da lista "numdados" o conteudo puxado em "pesquisadefinida" formatado em texto
 
-for linha in range(0,numpartidas):
-    for coluna in range(0,2):
-        numDadosTime1Time2[linha][coluna] = numdados[j]
+
+#Optei por utilizar o método de matriz(tabelas) para armazenar os dados
+for linha in range(0,numpartidas): # loop de linha percorrendo de 0 até o número de partidas
+    for coluna in range(0,2): # loop definindo que teremos somente 2 colunas
+        numDadosTime1Time2[linha][coluna] = numdados[j] #numdados acompanhando linha e coluna conforme. E armazenando o conteudo em cada.
         j = j + 1
 
 
 print(numDadosTime1Time2)
 
-with open('final.csv', 'a', newline="") as csvfile:
+with open('final.csv', 'a', newline="") as csvfile: #Escreve no arquivo csv já criado
 
     wr = csv.writer(csvfile,quoting=csv.QUOTE_ALL)
     for word in numDadosTime1Time2:
